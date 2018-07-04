34c29ae98788ecb649e5a4174eb536b436727181
gasymovrv <04gas90@mail.ru>
2018-07-04 18:18:04 +0400

    Примечания, пояснения и рефакторинг для упрощения понимания (Декоратор)

--- src/main/java/ru/unlimit/javapro/patterns/DecoratorApp.java
+++ src/main/java/ru/unlimit/javapro/patterns/DecoratorApp.java
@@ -2,22 +2,31 @@ package ru.unlimit.javapro.patterns;
 public class DecoratorApp {
 	public static void main(String[] args) {
 		PrinterInterface printer = new Printer("Hello");
-		PrinterInterface printerDecor1 = new QuotesDecorator(new QuotesDecorator(printer));
-		PrinterInterface printerDecor2 = new QuotesDecorator(new RightBracketDecorator(new LeftBracketDecorator(printer)));
+		PrinterInterface printerDecor1 = new QuotesDecorator(printer);
+		PrinterInterface printerDecor2 = new BracketDecorator(printer);
+		PrinterInterface printerDecor3 = new QuotesDecorator(new BracketDecorator(printer));
 
 		printer.print();
 		System.out.println();
+
 		printerDecor1.print();
 		System.out.println();
+
 		printerDecor2.print();
+		System.out.println();
+
+		printerDecor3.print();
 
 	}
 }
+//------------------------------------Components----------------------------------
 
+//Component или Decorator (Wrapper)
 interface PrinterInterface{
 	void print();
 }
 
+//ConcreteComponent
 class Printer implements PrinterInterface{
 	String value;
 	public Printer(String value) {this.value = value;}
@@ -27,77 +36,39 @@ class Printer implements PrinterInterface{
 }
 
 
+//------------------------------------Decorators----------------------------------
 
-class QuotesDecorator implements PrinterInterface{
-	PrinterInterface component;
-	public QuotesDecorator(PrinterInterface component) {
+//Decorator
+abstract class Decorator implements PrinterInterface{
+	private PrinterInterface component;
+	public Decorator(PrinterInterface component) {
 		this.component = component;
 	}
 	public void print() {
-		System.out.print("\"");
 		component.print();
+	}
+}
+
+//ConcreteDecorator 1
+class QuotesDecorator extends Decorator{
+	public QuotesDecorator(PrinterInterface component) {
+		super(component);
+	}
+	public void print() {
+		System.out.print("\"");
+		super.print();
 		System.out.print("\"");
 	}
 }
 
-class LeftBracketDecorator implements PrinterInterface{
-	PrinterInterface component;
-	public LeftBracketDecorator(PrinterInterface component) {
-		this.component = component;
+//ConcreteDecorator 2
+class BracketDecorator extends Decorator{
+	public BracketDecorator(PrinterInterface component) {
+		super(component);
 	}
 	public void print() {
 		System.out.print("[");
-		component.print();;
-	}
-}
-
-class RightBracketDecorator  implements PrinterInterface{
-	PrinterInterface component;
-	public RightBracketDecorator(PrinterInterface component) {
-		this.component = component;
-	}
-	public void print() {
-		component.print();
+		super.print();
 		System.out.print("]");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-//abstract class Decorator implements PrinterInterface{
-//	PrinterInterface component;
-//	public Decorator(PrinterInterface component) {this.component = component;}
-//	public void print() {
-//		component.print();
-//	}
-//}
-//class QuotesDecorator extends Decorator{
-//	public QuotesDecorator(PrinterInterface component) {super(component);}
-//	public void print() {
-//		System.out.print("\"");
-//		super.print();
-//		System.out.print("\"");
-//	}
-//}
-//class LeftBracketDecorator extends Decorator{
-//	public LeftBracketDecorator(PrinterInterface component) {super(component);}
-//	public void print() {
-//		System.out.print("(");
-//		super.print();
-//	}
-//}
-//class RightBracketDecorator extends Decorator{
-//	public RightBracketDecorator(PrinterInterface component) {super(component);}
-//	public void print() {
-//		super.print();
-//		System.out.print(")");
-//	}
-//}
