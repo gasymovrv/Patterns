8f44ab5a18bbae3d6b77256f5cf33a91b7ed1502
gasymovrv <04gas90@mail.ru>
2018-07-04 16:22:45 +0400

    Примечания, пояснения и рефакторинг для упрощения понимания (Одиночка)

--- src/main/java/ru/unlimit/javapro/patterns/SingletonApp.java
+++ src/main/java/ru/unlimit/javapro/patterns/SingletonApp.java
@@ -1,25 +1,27 @@
 package ru.unlimit.javapro.patterns;
 public class SingletonApp {
 	public static void main(String[] args) throws InterruptedException {
-		
 		final int SIZE = 1000;
-		
 		Thread t[] = new Thread[SIZE];
-				
+
+		//создаем кучу потоков
 		for(int i=0; i<SIZE; i++){
 			t[i] = new Thread(new R());
+		}
+		//Стартуем их все (в каждом вызывается Singleton.getInstance())
+		for(int i=0; i<SIZE; i++){
 			t[i].start();
 		}
+		//Ждем
 		for(int i=0; i<SIZE; i++){
 			t[i].join();
 		}
-		
+		//Проверям, что создался только один объект
 		System.out.println(Singleton.counter);
 		
 	}
 }
 class R implements Runnable{
-
 	@Override
 	public void run() {
 		Singleton.getInstance();
@@ -27,35 +29,16 @@ class R implements Runnable{
 }
 
 class Singleton{
-	private static final Object sync = new Object();
-	
 	public static int counter = 0;
 	private static volatile Singleton instance = null;
+
 	private Singleton(){
 		counter++;
 	}
-//	public Singleton getInstance(){
-//		return instance;
-//	}
-	
-//	public static Singleton getInstance(){
-//		if(instance == null){
-//			instance = new Singleton();
-//		}
-//		return instance;
-//	}
-//	public static synchronized Singleton getInstance(){
-//		if(instance == null){
-//			instance = new Singleton();
-//		}
-//		return instance;
-//	}
-	public static Singleton getInstance(){
+
+	public static synchronized Singleton getInstance(){
 		if(instance == null){
-			synchronized(sync){
-				if(instance == null)
-					instance = new Singleton();
-			}
+			instance = new Singleton();
 		}
 		return instance;
 	}
