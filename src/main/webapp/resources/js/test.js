f4f0de9f7cacc7bd6c79f0dd88b110750c0c9a08
gasymovrv <04gas90@mail.ru>
2018-08-02 18:50:40 +0400

    js-base : commit 1

---
+++ src/main/webapp/resources/js/test.js
@@ -0,0 +1,8 @@
+var arr = [1, 2, 3, 4, 5];
+// для каждого элемента массива запустить функцию,
+// промежуточный результат передавать первым аргументом далее
+var result = arr.reduce(function(sum, current) {
+    return sum + current;
+});
+
+alert( result ); // 15
