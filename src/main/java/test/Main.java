a2b444bb6366f218f9c6c17852c9cb366b532dc5
gasymovrv <04gas90@mail.ru>
2018-08-15 15:04:55 +0400

    java: test finally

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -2,8 +2,27 @@ package test;
 
 public class Main {
     public static void main(String[] args) {
+        //-----------------------------Math.random()-------------------
         for (int i = 0; i < 100; i++) {
             System.out.println((int)(Math.random()*5));
+        }
+
+        //-----------------------------finally-------------------
+        System.out.println(finallyTest());
+    }
+
+    private static String finallyTest(){
+        String message = null;
+        try {
+            message = "Успешно!";
+//            throw new Exception();
+            return message;
+        } catch (Exception e){
+            message = "Ошибка!";
+            return message;
+        } finally {
+            message += " from finally";
+            return message;
         }
     }
 }
