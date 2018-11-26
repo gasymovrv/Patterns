41949c0cdbf6e67a330a2601fa5ef423e7dda636
r.gasimov <ruslan.gasimov@artezio.com>
2018-11-26 17:14:18 +0400

    js-dom : commit 10 (координаты окна, страницы и евента)

--- src/main/webapp/resources/js/dom.js
+++ src/main/webapp/resources/js/dom.js
@@ -735,3 +735,39 @@ function highlightThis() {
     alert(this.tagName);
     this.style.backgroundColor = '';
 }
+
+
+
+
+
+//--------------------------------------------Координаты окна, страницы и евента----------------------------------------
+
+//При движении мыши показываются координаты и инфа о  теге на котором мышь сейчас
+document.body.addEventListener("mousemove", mouseMoveInfo);
+function mouseMoveInfo(event) {
+    var div = document.getElementById('mouseCoord');
+    if (!div) {
+        div = document.createElement('div');
+    }
+    div.setAttribute('id', 'mouseCoord');
+    div.style.position = "absolute";
+    div.style.padding = "3px";
+    div.style.border = "solid 2px";
+    div.style.backgroundColor = "#333434";
+    div.style.color = "#bdbdbd";
+    div.innerHTML = `clientX=${event.clientX},<br>
+                        clientY=${event.clientY},<br>
+                        pageX=${event.pageX},<br>
+                        pageY=${event.pageY},<br>
+                        targetTag=${event.target.tagName},<br>
+                        targetId=${event.target.getAttribute('id')},<br>
+                        targetClass=${event.target.getAttribute('class')}`;
+    //Ставим координаты элемента
+    //Вычитаем div.offsetHeight чтобы сдвинуть див вверх на свой размер
+    //и 3px чтобы было повыше курсора
+    //Можно и div.clientWidth/div.clientHeight но они не учитывают бордер
+    //event.clientX/event.clientY - это координаты окна, не учитывают прокрутку
+    div.style.left = `${event.pageX}px`;
+    div.style.top = `${event.pageY - div.offsetHeight - 3}px`;
+    document.body.appendChild(div);
+}
--- src/main/webapp/resources/js/testDOM.js
+++ src/main/webapp/resources/js/testDOM.js
@@ -1,16 +1,33 @@
 function run() {
 
-    var elems = document.querySelectorAll('#id19_1,#id19_2,#id19_3');
-
-    for (var i = 0; i < elems.length; i++) {
-        elems[i].addEventListener("click", highlightThis, true);
-        elems[i].addEventListener("click", highlightThis, false);
-    }
-
-    function highlightThis() {
-        this.style.backgroundColor = 'yellow';
-        alert(this.tagName);
-        this.style.backgroundColor = '';
+    //При движении мыши показываются координаты и инфа о  теге на котором мышь сейчас
+    document.body.addEventListener("mousemove", mouseMoveInfo);
+    function mouseMoveInfo(event) {
+        var div = document.getElementById('mouseCoord');
+        if(!div){
+            div = document.createElement('div');
+        }
+        div.setAttribute('id', 'mouseCoord');
+        div.style.position = "absolute";
+        div.style.padding = "3px";
+        div.style.border = "solid 2px";
+        div.style.backgroundColor = "#333434";
+        div.style.color = "#bdbdbd";
+        div.innerHTML = `clientX=${event.clientX},<br>
+                        clientY=${event.clientY},<br>
+                        pageX=${event.pageX},<br>
+                        pageY=${event.pageY},<br>
+                        targetTag=${event.target.tagName},<br>
+                        targetId=${event.target.getAttribute('id')},<br>
+                        targetClass=${event.target.getAttribute('class')}`;
+        //Ставим координаты элемента
+        //Вычитаем div.offsetHeight чтобы сдвинуть див вверх на свой размер
+        //и 3px чтобы было повыше курсора
+        //Можно и div.clientWidth/div.clientHeight но они не учитывают бордер
+        //event.clientX/event.clientY - это координаты окна, не учитывают прокрутку
+        div.style.left = `${event.pageX}px`;
+        div.style.top = `${event.pageY-div.offsetHeight-3}px`;
+        document.body.appendChild(div);
     }
 
 }
