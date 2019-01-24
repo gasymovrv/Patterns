067434aa456093cd631851b1e9b3807c5e184ff5
r.gasimov <ruslan.gasimov@artezio.com>
2019-01-24 16:39:59 +0400

    regex - самый простой пример жадных/ленивых

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,7 +1,6 @@
-//?! - исключение слов - для русских все сложнее, нет метасимвола для границы слов, поэтому нужны двойные пробелы
-//сделать [a-zа-яё]+ не используя границы слов не получится в связке исключающей группой
-var str = ' Se  cat  КОТовасия  Gecat  Po Po2  Po3  cat  Cat  Service  Catty  cat  кот  cat  cat  КОТ ';
-lg( str.match(/(\s|^)(?!cat[ ,.;:'"-]|кот[ ,.;:'"-])\S+(\s|$)/gi) ); // [ " Se ", " КОТовасия ", " Gecat ", " Po ", " Po3 ", " Service ", " Catty " ]
-
-var str2 = 'Se cat Gecat Po cat Service cat Cat Catty cat cat';
-lg( str2.match(/\b((?!cat\b)\w+)\b/gi) ); // [ "Se", "Gecat", "Po", "Service", "Catty" ]
+//Жадный/Ленивый самый простой пример
+var reg1 = /a+/g;
+var reg2 = /a+?/g;
+var str = 'aaaaaa';
+lg( str.match(reg1) ); // [ "aaaaaa" ]
+lg( str.match(reg2) ); // [ "a", "a", "a", "a", "a", "a" ]
