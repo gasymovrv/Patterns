d2eed0d80aa66c378f6a763990ac65b70e26b4c1
Ruslan Gasymov <RGasymov@luxoft.com>
2021-02-16 01:03:43 +0400

    Change TestUserGenerator to multithreaded

--- src/main/java/examples/apprecl/utils/TestUserGenerator.java
+++ src/main/java/examples/apprecl/utils/TestUserGenerator.java
@@ -8,79 +8,105 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import examples.utils.TimeMeasurementUtil;
 
 public class TestUserGenerator {
 
-    public static void main(String... args) throws Exception {
+    public static void main(String... args) {
         int max = Integer.parseInt("12000");
         if (max < 1) {
             throw new RuntimeException("Argument must be greater than 1");
         }
-        generateLdapUsers(max);
+        long time = TimeMeasurementUtil.measureTime(() -> {
+            try {
+                generateLdapUsers(max);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.printf("Generation took: %s ms", time);
     }
 
     private static void generateLdapUsers(int max) throws InterruptedException {
 
-        List<String> linesForLDAP = new ArrayList<>();
-        List<String> linesForSAML = new ArrayList<>();
+        StringBuffer ldapUsers = new StringBuffer();
+        StringBuffer samlUsers = new StringBuffer();
 
-        linesForLDAP.add("\t");
-        linesForSAML.add("\t");
         FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en-US"), new RandomService());
 
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
         System.out.println("# ------------ Started generation test users ---------------");
+
         for (int i = 1; i <= max; i++) {
-            Faker faker = new Faker();
-            Name name = faker.name();
+            final int x = i;
+            executorService.execute(() -> {
+                Faker faker = new Faker();
+                Name name = faker.name();
 
-            String cn;
-            if (0.1 <= Math.random()) {
-                cn = String.format("s%s", StringUtils.leftPad(String.valueOf(i), 6, '0'));
-            } else {
-                cn = String.format("s%sL", StringUtils.leftPad(String.valueOf(i), 6, '0'));
-            }
-            String sn = name.lastName();
-            String givenName = name.firstName();
-            String mail = (username(givenName, sn) + i + "@gmail.com");
-            String physicalDeliveryOfficeName = fakeValuesService.regexify("[A-Z]{3}");
-            String department = fakeValuesService.regexify("[A-Z]{3,7}");
+                String cn;
+                if (0.1 <= Math.random()) {
+                    cn = String.format("s%s", StringUtils.leftPad(String.valueOf(x), 6, '0'));
+                } else {
+                    cn = String.format("s%sL", StringUtils.leftPad(String.valueOf(x), 6, '0'));
+                }
+                String sn = name.lastName();
+                String givenName = name.firstName();
+                String mail = (username(givenName, sn) + x + "@gmail.com");
+                String physicalDeliveryOfficeName = fakeValuesService.regexify("[A-Z]{3}");
+                String department = fakeValuesService.regexify("[A-Z]{3,7}");
 
-            linesForLDAP.add(String.format("# Entry %d", i));
-            linesForLDAP.add(String.format("dn: cn=%s,ou=users,dc=testcompany,dc=com", cn));
-            linesForLDAP.add(String.format("cn: %s", cn));
-            linesForLDAP.add(String.format("sn: %s", sn));
-            linesForLDAP.add(String.format("givenname: %s", givenName));
-            linesForLDAP.add(String.format("mail: %s", mail));
-            linesForLDAP.add(String.format("physicalDeliveryOfficeName: %s", physicalDeliveryOfficeName));
-            linesForLDAP.add(String.format("department: %s", department));
-            linesForLDAP.add("objectclass: appreclPerson");
-            linesForLDAP.add("");
+                String ldapUser = "\n" +
+                        String.format("dn: cn=%s,ou=users,dc=testcompany,dc=com", cn) +
+                        "\n" +
+                        String.format("cn: %s", cn) +
+                        "\n" +
+                        String.format("sn: %s", sn) +
+                        "\n" +
+                        String.format("givenname: %s", givenName) +
+                        "\n" +
+                        String.format("mail: %s", mail) +
+                        "\n" +
+                        String.format("physicalDeliveryOfficeName: %s", physicalDeliveryOfficeName) +
+                        "\n" +
+                        String.format("department: %s", department) +
+                        "\n" +
+                        "objectclass: appreclPerson" +
+                        "\n";
+                ldapUsers.append(ldapUser);
 
-            linesForSAML.add(String.format("        '%s:password' => array_merge($test_user_base, array(", cn));
-            linesForSAML.add(String.format("            'Cn' => '%s'", cn));
-            linesForSAML.add("        )),");
+                String samlUser = "\n" +
+                        String.format("        '%s:password' => array_merge($test_user_base, array(", cn) +
+                        "\n" +
+                        String.format("            'Cn' => '%s'", cn) +
+                        "\n" +
+                        "        )),";
+                samlUsers.append(samlUser);
+            });
         }
 
-        linesForSAML.add("        'userWrong:password' => array_merge($test_user_base, array(");
-        linesForSAML.add("            'Cn' => 'userWrong'");
-        linesForSAML.add("        ))");
-        linesForSAML.add("    )");
-        linesForSAML.add(");");
+        executorService.shutdown();
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
+        System.out.println("All generate-users tasks completed");
 
+        samlUsers.append("\n        'userWrong:password' => array_merge($test_user_base, array(\n");
+        samlUsers.append("            'Cn' => 'userWrong'\n");
+        samlUsers.append("        ))\n");
+        samlUsers.append("    )\n");
+        samlUsers.append(");\n");
         System.out.println("# ------------ Test users were successfully generated ---------------");
 
-        writeToConfigFiles(linesForLDAP, linesForSAML);
+        writeToConfigFiles(
+                ldapUsers.toString().getBytes(),
+                samlUsers.toString().getBytes());
     }
 
-    private static void writeToConfigFiles(List<String> linesForLDAP, List<String> linesForSAML) throws InterruptedException {
+    private static void writeToConfigFiles(byte[] ldapUsers, byte[] samlUsers) throws InterruptedException {
         Path ldapConfigFile = Paths.get("src/main/java/examples/apprecl/utils/ldap-users.ldif");
         if (!Files.exists(ldapConfigFile)) {
             try {
@@ -99,35 +125,27 @@ public class TestUserGenerator {
         }
 
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.submit(() -> {
+        executorService.execute(() -> {
             try {
-                FileUtils.writeLines(ldapConfigFile.toFile(), linesForLDAP, true);
+                FileUtils.writeByteArrayToFile(ldapConfigFile.toFile(), ldapUsers, true);
                 System.out.println("# ------------ Test users were successfully written to LDAP config file ---------------");
             } catch (IOException e) {
                 System.out.println("# ------------ Error while writing test users to LDAP config file ---------------");
             }
         });
 
-        executorService.submit(() -> {
+        executorService.execute(() -> {
             try {
-                FileUtils.writeLines(samlConfigFile.toFile(), linesForSAML, true);
+                FileUtils.writeByteArrayToFile(samlConfigFile.toFile(), samlUsers, true);
                 System.out.println("# ------------ Test users were successfully written to SAML config file ---------------");
             } catch (IOException e) {
                 System.out.println("# ------------ Error while writing test users to SAML config file ---------------");
             }
         });
 
-        //Инициирует упорядоченное завершение работы,
-        // при котором ранее отправленные задачи выполняются, но новые задачи не принимаются.
         executorService.shutdown();
-        System.out.println("All tasks submitted");
-
-        //Что-то типо join, ждем наступления первого из:
-        // все задачи выполнились
-        // истек таймаут
-        // interruption текущего потока
-        executorService.awaitTermination(150, TimeUnit.SECONDS);
-        System.out.println("All tasks completed");
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
+        System.out.println("All write-to-file tasks completed");
     }
 
     private static String username(String firstName, String lastName) {
