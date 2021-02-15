654abed07bf078fa229c4e982e1b2d3648d8375d
Ruslan Gasymov <RGasymov@luxoft.com>
2021-02-15 23:57:12 +0400

    Fix TestUserGenerator

--- src/main/java/examples/apprecl/utils/TestUserGenerator.java
+++ src/main/java/examples/apprecl/utils/TestUserGenerator.java
@@ -2,12 +2,15 @@ package examples.apprecl.utils;
 
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,7 +20,7 @@ import org.apache.commons.lang3.StringUtils;
 public class TestUserGenerator {
 
     public static void main(String... args) throws Exception {
-        int max = Integer.parseInt("10");
+        int max = Integer.parseInt("12000");
         if (max < 1) {
             throw new RuntimeException("Argument must be greater than 1");
         }
@@ -31,32 +34,34 @@ public class TestUserGenerator {
 
         linesForLDAP.add("\t");
         linesForSAML.add("\t");
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en-US"), new RandomService());
 
         System.out.println("# ------------ Started generation test users ---------------");
         for (int i = 1; i <= max; i++) {
             Faker faker = new Faker();
             Name name = faker.name();
 
-            String cn = String.valueOf(1000 + i);
+            String cn;
+            if (0.1 <= Math.random()) {
+                cn = String.format("s%s", StringUtils.leftPad(String.valueOf(i), 6, '0'));
+            } else {
+                cn = String.format("s%sL", StringUtils.leftPad(String.valueOf(i), 6, '0'));
+            }
             String sn = name.lastName();
             String givenName = name.firstName();
             String mail = (username(givenName, sn) + i + "@gmail.com");
-            String physicalDeliveryOfficeName = faker.address().fullAddress();
+            String physicalDeliveryOfficeName = fakeValuesService.regexify("[A-Z]{3}");
+            String department = fakeValuesService.regexify("[A-Z]{3,7}");
 
             linesForLDAP.add(String.format("# Entry %d", i));
-            linesForLDAP.add(String.format("dn: uid=%d,ou=users,dc=testcompany,dc=com", i));
+            linesForLDAP.add(String.format("dn: cn=%s,ou=users,dc=testcompany,dc=com", cn));
             linesForLDAP.add(String.format("cn: %s", cn));
             linesForLDAP.add(String.format("sn: %s", sn));
             linesForLDAP.add(String.format("givenname: %s", givenName));
             linesForLDAP.add(String.format("mail: %s", mail));
             linesForLDAP.add(String.format("physicalDeliveryOfficeName: %s", physicalDeliveryOfficeName));
-            linesForLDAP.add(String.format("uid: %d", i));
-            linesForLDAP.add(String.format("uidnumber: 100%d", i));
-            linesForLDAP.add(String.format("gidnumber: 50%d", i));
-            linesForLDAP.add(String.format("homedirectory: /home/users/%d", i));
-            linesForLDAP.add("objectclass: inetOrgPerson");
-            linesForLDAP.add("objectclass: posixAccount");
-            linesForLDAP.add("objectclass: top");
+            linesForLDAP.add(String.format("department: %s", department));
+            linesForLDAP.add("objectclass: appreclPerson");
             linesForLDAP.add("");
 
             linesForSAML.add(String.format("        '%s:password' => array_merge($test_user_base, array(", cn));
@@ -72,7 +77,11 @@ public class TestUserGenerator {
 
         System.out.println("# ------------ Test users were successfully generated ---------------");
 
-        Path ldapConfigFile = Paths.get("src/main/java/examples/apprecl/utils/ldap-testcompany.ldif");
+        writeToConfigFiles(linesForLDAP, linesForSAML);
+    }
+
+    private static void writeToConfigFiles(List<String> linesForLDAP, List<String> linesForSAML) throws InterruptedException {
+        Path ldapConfigFile = Paths.get("src/main/java/examples/apprecl/utils/ldap-users.ldif");
         if (!Files.exists(ldapConfigFile)) {
             try {
                 Files.createFile(ldapConfigFile);
